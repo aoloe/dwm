77306976caf1723358b407fdcea032ca91c782fc
ale rimoldi <ale@graphicslab.org>
2022-10-13 17:56:45 +0200

    set prefix path

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 6.4
 # Customize below to fit your system
 
 # paths
-PREFIX = /home/ale/bin/dwm
+PREFIX = /home/ale/bin/suckless-dwm
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
