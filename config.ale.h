51595e105d0f8aadf10fd2c279df63b162533a1b
ale rimoldi <ale@graphicslab.org>
2022-10-29 13:20:59 +0200

    a better way to avoid that new windows always appear on the main monitor

--- config.ale.h
+++ config.ale.h
@@ -22,7 +22,8 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {0};
+// static const Rule rules[] = {0};
+static const Rule rules[] = { { .monitor = -1 } };
 // static const Rule rules[] = {
 // 	/* xprop(1):
 // 	 *	WM_CLASS(STRING) = instance, class
